b4d5b9557720942dc69e70f140b784680707d8e2
Samuel Ward <samuelleeward9@gmail.com>
2022-11-28 12:06:04 +0100

    input validation added

--- classes/Studies.class.php
+++ classes/Studies.class.php
@@ -30,19 +30,38 @@ class Studies {
     }
 
     public function create() {
-        // SQL-fråga för att skapa en kurs med specifika värden
-        $sql = "INSERT INTO studies(school, name, type, startDate, endDate) VALUES('$this->school', '$this->name', '$this->type', '$this->startDate', '$this->endDate');";
-        $result = mysqli_query($this->db, $sql);
+        // Kollar om en sträng innehåller PHP eller HTML-kod
+        function isHTML($string) {
+            return $string != strip_tags($string) ? true:false;
+        }
 
-        return $result;
+        // Kollar att alla fält är ifyllda
+        if ($this->school && $this->name && $this->type && $this->startDate && $this->endDate) {
+            // Ser till att ingen kod injiceras
+            if (!isHTML($this->school) && !isHTML($this->name) && !isHTML($this->type) && !isHTML($this->startDate) && !isHTML($this->endDate)) {
+                // SQL-fråga för att skapa en kurs med specifika värden
+                $sql = "INSERT INTO studies(school, name, type, startDate, endDate) VALUES('$this->school', '$this->name', '$this->type', '$this->startDate', '$this->endDate');";
+                $result = mysqli_query($this->db, $sql);
+        
+                return $result;
+            }
+        }
     }
 
     public function update($id) {
-        // SQL-fråga för att uppdatera en specifik kurs
-        $sql = "UPDATE studies SET school = '$this->school', name = '$this->name', type = '$this->type', startDate = '$this->startDate', endDate = '$this->endDate' WHERE id = $id";
-        $result = mysqli_query($this->db, $sql);
+        function isHTML($string) {
+            return $string != strip_tags($string) ? true:false;
+        }
 
-        return $result;
+        if ($this->school && $this->name && $this->type && $this->startDate && $this->endDate) {
+            if (!isHTML($this->school) && !isHTML($this->name) && !isHTML($this->type) && !isHTML($this->startDate) && !isHTML($this->endDate)) {
+                // SQL-fråga för att uppdatera en specifik kurs
+                $sql = "UPDATE studies SET school = '$this->school', name = '$this->name', type = '$this->type', startDate = '$this->startDate', endDate = '$this->endDate' WHERE id = $id";
+                $result = mysqli_query($this->db, $sql);
+
+                return $result;
+            }
+        }
     }
 
     public function delete($id) {
